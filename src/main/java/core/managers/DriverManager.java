6ff09e5a9c65c607c9c358287d50def47959c682
KhanhNXB-MC <khanhnxb-mc@khanhNXB-MC.local>
2017-11-28 19:29:24 +0700

    Second commit

--- src/main/java/core/managers/DriverManager.java
+++ src/main/java/core/managers/DriverManager.java
@@ -1,4 +1,5 @@
 package core.managers;
 
 public class DriverManager {
+
 }
--- src/main/java/core/managers/ParallelManager.java
+++ src/main/java/core/managers/ParallelManager.java
@@ -1,4 +1,29 @@
 package core.managers;
 
+
+import org.openqa.selenium.remote.service.DriverService;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ParallelManager {
+    private Map<String, Object> driverPool = new HashMap<String, Object>();
+    private Map<String, DriverService> servicePool = new HashMap<String, DriverService>();
+
+
+    public void setDriver(Object driver) {
+        driverPool.put(Thread.currentThread().getName(), driver);
+    }
+
+    public Object getDriver() {
+        return driverPool.get(Thread.currentThread().getName());
+    }
+
+    public void setAppiumService(DriverService service) {
+        servicePool.put(Thread.currentThread().getName(), service);
+    }
+
+    public DriverService getAppiumService() {
+        return servicePool.get(Thread.currentThread().getName());
+    }
 }
